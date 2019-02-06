fe9dcb251eaa9b01a042605e07a0ca099aa4f2b4
Dave Ansell <dave@sciansell.co.uk>
2019-02-06 09:44:40 +0000

    fix some naming errors

--- node_helper.js
+++ node_helper.js
@@ -1,7 +1,9 @@
 var NodeHelper = require("node_helper");
+var doneSetup = false;
 module.exports = NodeHelper.create({
   socketNotificationReceived: function(notification, payload) {
     this.lights;
+    var thethis=this;
     console.log("MMM-lights notification ="+notification.toString());
     if(notification =="LIGHTS_START"){
 	console.log("MMM-lights starting");
@@ -12,17 +14,20 @@ module.exports = NodeHelper.create({
 	                        __dirname + "/lights.py",
 
 	]);
-	lights.stdin.setEncoding('utf-8');
-	lights.stdin.write("0\n");
-    	child.stdout.on("exit", function(exitCode) {
-    	  lights.stdin.write("q\n");
+	this.lights.stdin.setEncoding('utf-8');
+	this.lights.stdin.write("0\n");
+    	this.lights.stdout.on("exit", function(exitCode) {
+    	  thethis.lights.stdin.write("q\n");
     	  console.log(exitCode);
     	});
-    }else if(notification == "LIGHTS_SET"){
+	doneSetup = true;
+    }else if(notification == "LIGHTS_SET" && doneSetup){
 		if(payload==1){
 	    		this.lights.stdin.write("1\n");
+			console.log("lights on");
 		}else if(payload==0){
 	    		this.lights.stdin.write("0\n");
+			console.log("lights off");
 		}
 	}
   },
